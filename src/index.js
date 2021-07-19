d5d07d26ad9a0db54de977199feffafd3a416b1c
jmhar <jmhar@protonmail.com>
2021-07-19 12:50:52 +1000

    create to do list, attempt close button

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.getElementById('create-task-form');
+  let tasks = document.getElementById('tasks');
+
+  form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    let input = document.getElementById('new-task-description').value;
+    let entry = document.createElement('li');
+    // let button = document.createElement("button");
+    // button.innerHTML = '\u00D7';
+    // button.className = 'close';
+    entry.appendChild(document.createTextNode(input));
+    // entry.appendChild(button);
+    tasks.appendChild(entry);
+    document.getElementById('new-task-description').value = "";
+  })
+
+  // let listItems = document.getElementsByTagName('li');
+  // let i;
+  // for (i = 0; i < listItems.length; i++) {
+  //   let span = document.createElement('span');
+  //   let x = document.createTextNode('\u00D7');
+  //   span.className = 'close';
+  //   span.appendChild(x);
+  //   listItems[i].appendChild(span);
+  // }
+
+
+  let close = document.getElementsByClassName('close');
+
+  close.addEventListener('x', function (e) {
+    let div = this.parentElement;
+    div.style.display = 'none';
+  })
 });
